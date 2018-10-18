ce7a3a3142291e58cf27976eea7f2f41bf516050
nemanja.rasajski <nemanjarns@gmail.com>
2018-10-18 15:42:06 +0200

    Vuex persist added

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex);
 
@@ -7,11 +8,17 @@ import userBase from "./userBase"
 import invoiceStore from "./invoiceStore"
 import session from "./session"
 
+const vuexLocalStorage = new VuexPersistence({
+  key: 'vuex',
+  storage: window.localStorage, 
+})
+
 export const store  = new Vuex.Store({
     modules:
     {
       login: userBase,
       invoices : invoiceStore,
       session: session
-    }
+    },
+    plugins: [vuexLocalStorage.plugin]
 });
